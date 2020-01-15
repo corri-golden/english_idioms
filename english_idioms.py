85a372cbc2e6ebc8311edfc126bd4bf6aae57b13
Corri Golden <corri.golden@gmail.com>
2020-01-15 14:03:59 -0600

    first commit

---
+++ english_idioms.py
@@ -0,0 +1,18 @@
+idioms = {
+    "Penny": ["A", "penny", "for", "your", "thoughts"],
+    "Injury": ["Add", "insult", "to", "injury"],
+    "Moon": ["Once", "in", "a", "blue", "moon"],
+    "Grape": ["I", "heard", "it", "through", "the", "grapevine"],
+    "Murder": ["Kill", "two", "birds", "with", "one", "stone"],
+    "Limbs": ["It", "costs", "an", "arm", "and", "a", "leg"],
+    "Grain": ["Take", "what", "someone", "says", "with", "a", "grain", "of", "salt"],
+    "Fences": ["I'm", "on", "the", "fence", "about", "it"],
+    "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
+    "Lucifer": ["Speak", "of", "the", "devil"],
+}
+
+for idiom in idioms:     #square bracket is how you access the value in the key.
+    print(f'{idiom}: {" ".join(idioms[idiom])}')
+
+
+    
